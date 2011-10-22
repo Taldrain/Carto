4ea76005b2b41c3f74665a5e8761c377bc707797
Alonso <girald_a@pc-girald-a.(none)>
2011-10-22 17:58:00 +0200

    qq

--- pre.ml
+++ pre.ml
@@ -1,22 +1,6 @@
 (* BEGIN -- Functions for the pre traitement *)
 (* PROJET CARTOGRAPHIE  *)
 (* pre-traitement & quadrillage *)
-
-
-
-(* variables globales *)
-let w = ref 600
-let h = ref 600
-	
-let matrice = ref (Array.make_matrix 
-		     ((!w)/(Refe.get_step()) + 1) 
-		     ((!h)/(Refe.get_step()) + 1) (0,0))
-
-
-let matrice_rgb = ref (Array.make_matrix 
-			 ((!w)/((Refe.get_step())) + 1) 
-			 ((!h)/((Refe.get_step())) + 1)
-			 ((0,0),(0,0,0)))
   
 
 (* FONCTIONS *)   
@@ -24,8 +8,8 @@ let matrice_rgb = ref (Array.make_matrix
 (* Dimensions d'une image *)
 let get_dims img =
   begin
-    w := (Sdlvideo.surface_info img).Sdlvideo.w;
-    h := (Sdlvideo.surface_info img).Sdlvideo.h;
+    Refe.w := (Sdlvideo.surface_info img).Sdlvideo.w;
+    Refe.h := (Sdlvideo.surface_info img).Sdlvideo.h;
   end
     
 (* init de SDL *)
@@ -86,13 +70,13 @@ let contour image image2 =
     and down = ref (0,0,0)
     and center = ref (0,0,0) 
     and listcolor = ref [] in
-      for x=0 to !w-1 do
-	for y=0 to !h-1 do
+      for x=0 to (Refe.get_w())-1 do
+	for y=0 to (Refe.get_h())-1 do
 	  center := Sdlvideo.get_pixel_color image x y;
 	  right := Sdlvideo.get_pixel_color image (x+1) y;
 	  down := Sdlvideo.get_pixel_color image x (y+1);
-	  if (!center <> !right &&  x < !w-1 
-	      || !center <> !down &&  y < !h-1 ) then 
+	  if (!center <> !right &&  x < (Refe.get_w())-1 
+	      || !center <> !down &&  y < (Refe.get_h())-1 ) then 
 	    Sdlvideo.put_pixel_color image2 x y (0,0,0) else 
 	      Sdlvideo.put_pixel_color image2 x y !center;
 	  if (!center <> !right || !center <> !down) 
@@ -108,9 +92,9 @@ let contour image image2 =
 (* quadrillage horizontal *)
  let contour_hor image interv_y = 
    begin    
-       for y=0 to !h-1 do
+       for y=0 to (Refe.get_h())-1 do
 	 if y mod interv_y = 0 then 
-	   for x=0 to !w-1 do
+	   for x=0 to (Refe.get_w())-1 do
 	   Sdlvideo.put_pixel_color image x y (0,0,0);
 	   done;
        done;
@@ -119,9 +103,9 @@ let contour image image2 =
 (* quadrillage vertical *)
  let contour_ver image interv_x = 
    begin    
-     for x=0 to !w-1 do	     
+     for x=0 to (Refe.get_w())-1 do	     
        if x mod interv_x = 0 then 
-	 for y=0 to !h-1 do
+	 for y=0 to (Refe.get_h())-1 do
 	   Sdlvideo.put_pixel_color image x y (0,0,0);
 	 done;
      done;
@@ -131,11 +115,11 @@ let contour image image2 =
  let contour_diag1 image interv = 
    let rec con_d img x y inc_x inc_y y_finish = 
      match (x, y, inc_x, inc_y, y_finish) with
-	 (x, y, inc_x, inc_y, y_finish) when inc_x > !w-1 -> ()
-       | (x, y, inc_x, inc_y, y_finish) when inc_y > !h-1 
+	 (x, y, inc_x, inc_y, y_finish) when inc_x > (Refe.get_w())-1 -> ()
+       | (x, y, inc_x, inc_y, y_finish) when inc_y > (Refe.get_h())-1 
 	   -> con_d img 0 0 0 0 true
-       | (x, y, inc_x, inc_y, y_finish) when  x >= !w-1 || y >= !h-1
-	   -> if inc_x < !w-1 && y_finish = true then 
+       | (x, y, inc_x, inc_y, y_finish) when  x >= (Refe.get_w())-1 || y >= (Refe.get_h())-1
+	   -> if inc_x < (Refe.get_w())-1 && y_finish = true then 
 	     con_d img inc_x inc_y (inc_x + interv) 0 true else 
 	       if inc_x = 0 && y_finish = false then
 		 con_d img inc_x inc_y 0 (inc_y + interv) false
@@ -148,10 +132,10 @@ let contour image image2 =
 (* let contour_diag2 image interv = 
    let rec con_d img x y inc_x inc_y  = 
      match (x, y, inc_x, inc_y) with
-	 (x, y, inc_x, inc_y) when inc_x >= !w-1 -> ()
-       | (x, y, inc_x, inc_y) when inc_y >= !h-1 && x > !w-1
+	 (x, y, inc_x, inc_y) when inc_x >= (Refe.get_w())-1 -> ()
+       | (x, y, inc_x, inc_y) when inc_y >= (Refe.get_h())-1 && x > (Refe.get_w())-1
 	   -> con_d img inc_x inc_y (inc_x+interv) inc_y 
-       | (x, y, inc_x, inc_y) when  inc_y < !h-1 && (y <= 0)
+       | (x, y, inc_x, inc_y) when  inc_y < (Refe.get_h())-1 && (y <= 0)
 	   ->  con_d img 0 inc_y 0 (inc_y+interv) 
        | (x, y, inc_x, inc_y) 
 	 -> Sdlvideo.put_pixel_color img x y (0,0,0);
@@ -166,20 +150,20 @@ let contour image image2 =
 
 
  let rec map_to_mat x y intx inty = match (intx, inty) with
-   | (intx, inty) when intx > (!w) ->
+   | (intx, inty) when intx > ((Refe.get_w())) ->
        map_to_mat 0 (y+1) 0 (inty + (Refe.get_step())) 
-   | (intx, inty) when inty > (!h) -> () 
-   | (intx, inty) -> Array.set !matrice.(x) (y) (inty,intx); map_to_mat
+   | (intx, inty) when inty > ((Refe.get_h())) -> () 
+   | (intx, inty) -> Array.set (Refe.get_matrice()).(x) (y) (inty,intx); map_to_mat
        (x+1) y (intx+((Refe.get_step()))) inty
        
 
  
  let matXY_to_matRGB img = 
    begin  
-     for y = 0 to ((!h)/((Refe.get_step()))) do
-       for x = 0 to ((!w)/((Refe.get_step()))) do
- 	 let (a,b) = !matrice.(x).(y) in 
-           Array.set !matrice_rgb.(x) (y) 
+     for y = 0 to (((Refe.get_h()))/((Refe.get_step()))) do
+       for x = 0 to (((Refe.get_w()))/((Refe.get_step()))) do
+ 	 let (a,b) = (Refe.get_matrice()).(x).(y) in 
+           Array.set (Refe.get_matrice_rgb()).(x) (y) 
  	     ((x,y),(Sdlvideo.get_pixel_color img a b))
        done;
      done;
@@ -200,33 +184,33 @@ let pre_trait () =
     let img = Sdlloader.load_image (Refe.get_filename ()) in
       (* On récupère les dimensions *)
       get_dims img;
-      let img2 = Sdlvideo.create_RGB_surface_format img [] !w !h in
-      (* On crée la surface d'affichage *)
-      let display = Sdlvideo.set_video_mode !w !h [] in
-      (* on affiche l'image avant traitement*)
-      show img display;
-      (* on attend une touche *)
-      wait_key();
-      (* on appelle la fonction de pretraitement*)
-      contour img img2;
-      show img2 display; 
-      wait_key ();
-      (* fonction de quadrillage *)
-      contour_hor img2 (Refe.get_step());
-      contour_ver img2 (Refe.get_step());
-      contour_diag1 img2 (Refe.get_step());
-      map_to_mat 0 0 0 0;
-      matXY_to_matRGB img;
-      (*  contour_diag2 img2 interv; *)
-      (* on affiche l'image apres traitement*)
-      show img2 display; 
-      (* enregistrement *) 
-      Sdlvideo.save_BMP img2 "out.bmp";
-
-      (* on attend une touche *)
-      wait_key ();
-      (* on quitte *)
-      Sdl.quit ();
+      let img2 = Sdlvideo.create_RGB_surface_format 
+	img [] (Refe.get_w()) (Refe.get_h()) in
+	(* On crée la surface d'affichage *)
+      let display = Sdlvideo.set_video_mode (Refe.get_w()) (Refe.get_h()) [] in
+	(* on affiche l'image avant traitement*)
+	show img display;
+	(* on attend une touche *)
+	wait_key();
+	(* on appelle la fonction de pretraitement*)
+	contour img img2;
+	show img2 display; 
+	wait_key ();
+	(* fonction de quadrillage *)
+	contour_hor img2 (Refe.get_step());
+	contour_ver img2 (Refe.get_step());
+	contour_diag1 img2 (Refe.get_step());
+	map_to_mat 0 0 0 0;
+	matXY_to_matRGB img;
+	(*  contour_diag2 img2 interv; *)
+	(* on affiche l'image apres traitement*)
+	show img2 display; 
+	(* enregistrement *) 
+	Sdlvideo.save_BMP img2 "out.bmp";
+	(* on attend une touche *)
+	wait_key ();
+	(* on quitte *)
+	Sdl.quit ();
 	Refe.pos := 2
   end
 (* END -- Functions for the pre traitement *)
