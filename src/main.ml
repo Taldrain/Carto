02d8a7491c1624f8edba73dda7cc05359c87bfe4
tommytom <thomas_pro-security@hotmail.com>
2011-11-25 20:16:58 +0100

    Nouvelle ancienne interface graphique

--- src/main.ml
+++ src/main.ml
@@ -29,14 +29,10 @@ let main () =
 	ignore (GtkMain.Main.init ());
 
 	(* Recuperation de la taille de l'ecran *)
-	let screen = Gdk.Screen.default () in
-	let screen_hei = Gdk.Screen.height ~screen:screen () in
-	let screen_wid = Gdk.Screen.width ~screen:screen () in
-
   	let w = GWindow.window
 		~title:"Carto TopoTeam" ()
-		~width:screen_wid
-		~height:screen_hei
+		(*~width:200
+		~height:400*)
 		~position:`CENTER in
 	let main_box = GPack.vbox
 		~packing:w#add () in
@@ -56,43 +52,30 @@ let main () =
 		~packing:menubar#append () in
 	item1#set_submenu menufile;
 
-	(* PANEL *)
-	let pan = GPack.paned `HORIZONTAL
-		~border_width:5
-		~width:screen_wid
-		~height:(screen_hei - 50)
-		~packing:main_box#add () in
-	(* Panel left *)
-	let box = GPack.vbox
-		~packing:pan#add1 () in
 	let _lbl = GMisc.label
 		~text:"Projet Carto -- Topo team"
-		~packing:box#add () in
+		~packing:main_box#pack () in
 	
 	let _lbl = GMisc.label
 		~text:"\nEffectuer le pre traitement\navant de lancer l'assistant\n"
-		~packing:box#add () in
+		~packing:main_box#pack () in
 	let btn_browse = GButton.button
 		~label:"Browse"
-		~packing:box#add () in
+		~packing:main_box#pack () in
 	let btn_pre_treat = GButton.button
 		~label:"Execute"
-		~packing:box#add () in
+		~packing:main_box#pack () in
 	let btn_assist = GButton.button
 		~label:"Assist"
-		~packing:box#add () in
+		~packing:main_box#pack () in
 	let btn_quit = GButton.button
 		~label:"Quit"
-	 	~packing:box#add () in
+	 	~packing:main_box#pack () in
 
 	(* Panel right *)
-	let box = GPack.vbox
-		~packing:pan#add2 () in
-	(*let area = GlGtk.area []
-		~packing:box#add () in*)
-	let btn_test = GButton.button
+	(*let btn_test = GButton.button
 		~label:"Test"
-		~packing:box#add () in
+		~packing:box#add () in*)
 
 	btn_pre_treat#misc#set_sensitive false;
 	btn_assist#misc#set_sensitive false;
