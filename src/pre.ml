f008699275840eeaf4af8f34b1528e80f727de0c
alonso giraldo <girald_a@pc-girald-a.(none)>
2011-12-03 01:59:00 +0100

    pre.ml

--- src/pre.ml
+++ src/pre.ml
@@ -1,160 +1,163 @@
 (* BEGIN -- Functions for the pre traitement *)
 (* PROJET CARTOGRAPHIE  *)
-(* pre-traitement & quadrillage *)
+(* pre-treatment & quadrillage *)
 
 
-(* FONCTIONS *)
+(* -------------------------- Sdl initialization ---------------------------- *)
 
-(* Dimensions d'une image *)
+(* Size of an image *)
 let get_dims img =
-  begin
-    Refe.w := (Sdlvideo.surface_info img).Sdlvideo.w;
-    Refe.h := (Sdlvideo.surface_info img).Sdlvideo.h;
-  end
+  Refe.w := (Sdlvideo.surface_info img).Sdlvideo.w;
+  Refe.h := (Sdlvideo.surface_info img).Sdlvideo.h
 
-(* init de SDL *)
+(* SDL init *)
 let sdl_init () =
-  begin
-    Sdl.init [`EVERYTHING];
-    Sdlevent.enable_events Sdlevent.all_events_mask;
-  end
+  Sdl.init [`EVERYTHING];
+  Sdlevent.enable_events Sdlevent.all_events_mask
 
-(* attendre une touche ... *)
+(* wait a key ... *)
 let rec wait_key () =
   let e = Sdlevent.wait_event () in
     match e with
-	Sdlevent.KEYDOWN _ -> ()
+	    | Sdlevent.KEYDOWN _ -> ()
       | _ -> wait_key ()
 
-(* affiche la surface img sur la surface de destination dst
-   (normalement l'écran) *)
+      
+(* shows the surface of the image on the destination surface dst (the screen) *)
 let show img dst =
   let d = Sdlvideo.display_format img in
     Sdlvideo.blit_surface d dst ();
     Sdlvideo.flip dst
 
 
-(* fonction qui analyse si l'element est dans la liste, si oui
-   le rajoute dans celle ci *)
+
+(* ------------------------------------ ------------------------------------- *)
+
+(* ------------------- Write colors in a .txt --------------------------------*)
+
+(* fnction analyze if the elemnt is in tje list if true add the elmnt in this *)
 let rec p_list li pix =
   begin
     match li with
-	[] -> [pix]
-      | e::li when e<>pix -> e::(p_list li pix)
+	    | [] -> [pix]
+      | e::li when e <> pix -> e::(p_list li pix)
       | _ -> li
   end
 
-(* (int*int*int) -> string *)
+(*    (int*int*int) -> string    *)
 let str_of_tri tr = let (a,b,c) = tr in
   "("^string_of_int(a)^","^string_of_int(b)^","^string_of_int(c)^")\n"
 
-(* (int*int*int)list -> string list *)
+(*    (int*int*int)list -> string list    *)
 let rec stringList li = match li with
     [] -> []
   | e::li -> (str_of_tri e)::(stringList li)
 
-(* fonction qui ecrit une liste dans un .txt, le creer si
-   inexistant et le ferme *)
+(* fnction which write a list in a .txt, create if it doesn't exist nd close *)
 let list_to_text li =
-  begin
-    let f = open_out "InfoCarto.txt" in
-    let stringLi = stringList li in
-      List.iter (output_string f) stringLi;
-      close_out f;
-  end
+  let f = open_out "InfoCarto.txt" in
+  let stringLi = stringList li in
+    List.iter (output_string f) stringLi;
+    close_out f
 
-(* fonction contour *)
+
+
+(* ------------------------------------ ------------------------------------- *)
+
+(* ---------------------------- Edge detection ------------------------------ *)
+
+(* Edge function *)
 let contour image image2 =
-  begin
-    let right = ref (0,0,0)
-    and down = ref (0,0,0)
-    and center = ref (0,0,0)
-    and listcolor = ref [] in
-      for x=0 to (Refe.get_w())-1 do
-	for y=0 to (Refe.get_h())-1 do
-	  center := Sdlvideo.get_pixel_color image x y;
-	  right := Sdlvideo.get_pixel_color image (x+1) y;
-	  down := Sdlvideo.get_pixel_color image x (y+1);
-	  if (!center <> !right &&  x < (Refe.get_w())-1
-	      || !center <> !down &&  y < (Refe.get_h())-1 ) then
-	    Sdlvideo.put_pixel_color image2 x y (0,0,0) else
-	      Sdlvideo.put_pixel_color image2 x y !center;
-	  if (!center <> !right || !center <> !down)
-	  then listcolor := p_list !listcolor !center
-	  else() ;
-	done;
+  let right = ref (0,0,0)
+  and down = ref (0,0,0)
+  and center = ref (0,0,0)
+  and listcolor = ref [] in
+    for x=0 to (Refe.get_w())-1 do
+      for y=0 to (Refe.get_h())-1 do
+        center := Sdlvideo.get_pixel_color image x y;
+        right := Sdlvideo.get_pixel_color image (x+1) y;
+        down := Sdlvideo.get_pixel_color image x (y+1);
+        if (!center <> !right &&  x < (Refe.get_w())-1
+        || !center <> !down &&  y < (Refe.get_h())-1 ) then
+          Sdlvideo.put_pixel_color image2 x y (0,0,0) else
+          Sdlvideo.put_pixel_color image2 x y !center;
+        if (!center <> !right || !center <> !down) then 
+          listcolor := p_list !listcolor !center
+        else() ;
       done;
-      Refe.li := !listcolor;
-      list_to_text !listcolor;
-  end
+    done;
+    Refe.li := !listcolor;
+    (* call fct which write colors in a .txt *)
+    list_to_text !listcolor
 
 
-(* quadrillage horizontal *)
- let contour_hor image interv_y =
-   begin
-       for y=0 to (Refe.get_h())-1 do
-	 if y mod interv_y = 0 then
+
+(* ------------------------------------ ------------------------------------- *)
+
+(* --------------------------------- Grid ----------------------------------- *)
+
+(* horizontal grid *)
+let contour_hor image interv_y =
+  for y=0 to (Refe.get_h())-1 do
+	  if y mod interv_y = 0 then
 	   for x=0 to (Refe.get_w())-1 do
-	   Sdlvideo.put_pixel_color image x y (0,0,0);
+	     Sdlvideo.put_pixel_color image x y (0,0,0);
 	   done;
-       done;
-   end
+  done
 
-(* quadrillage vertical *)
- let contour_ver image interv_x =
-   begin
-     for x=0 to (Refe.get_w())-1 do
-       if x mod interv_x = 0 then
-	 for y=0 to (Refe.get_h())-1 do
-	   Sdlvideo.put_pixel_color image x y (0,0,0);
-	 done;
-     done;
-   end
+(* vertical grid*)
+let contour_ver image interv_x =
+  for x=0 to (Refe.get_w())-1 do
+    if x mod interv_x = 0 then
+	    for y=0 to (Refe.get_h())-1 do
+	      Sdlvideo.put_pixel_color image x y (0,0,0);
+	    done;
+  done
 
-(* quadrillage diagonal gauchehaut-droitebas *)
- let contour_diag1 image interv =
-   let rec con_d img x y inc_x inc_y y_finish =
-     match (x, y, inc_x, inc_y, y_finish) with
-	 (x, y, inc_x, inc_y, y_finish) when inc_x > (Refe.get_w())-1 -> ()
-       | (x, y, inc_x, inc_y, y_finish) when inc_y > (Refe.get_h())-1
-	   -> con_d img 0 0 0 0 true
-       | (x, y, inc_x, inc_y, y_finish) when  x >= (Refe.get_w())-1 || y >= (Refe.get_h())-1
-	   -> if inc_x < (Refe.get_w())-1 && y_finish = true then
-	     con_d img inc_x inc_y (inc_x + interv) 0 true else
-	       if inc_x = 0 && y_finish = false then
-		 con_d img inc_x inc_y 0 (inc_y + interv) false
-       | (x, y, inc_x, inc_y, y_finish)
-	 -> Sdlvideo.put_pixel_color img x y (0,0,0);
-	   con_d img (x+1) (y+1) inc_x inc_y y_finish
-   in con_d image 0 0 0 0 false
+(* Diagonal grid left-up/right-down *)
+let contour_diag1 image interv =
+  let rec con_d img x y inc_x inc_y y_finish =
+    match (x, y, inc_x, inc_y, y_finish) with
+	   | (x, y, inc_x, inc_y, y_finish) when inc_x > (Refe.get_w())-1 -> ()
+     | (x, y, inc_x, inc_y, y_finish) when inc_y > (Refe.get_h())-1 -> 
+         con_d img 0 0 0 0 true
+     | (x, y, inc_x, inc_y, y_finish) 
+       when  x >= (Refe.get_w())-1 || y >= (Refe.get_h())-1 -> 
+         if inc_x < (Refe.get_w())-1 && y_finish = true then
+	          con_d img inc_x inc_y (inc_x + interv) 0 true 
+          else if inc_x = 0 && y_finish = false then
+		        con_d img inc_x inc_y 0 (inc_y + interv) false
+     | (x, y, inc_x, inc_y, y_finish) -> 
+         Sdlvideo.put_pixel_color img x y (0,0,0);
+	       con_d img (x+1) (y+1) inc_x inc_y y_finish
+    in con_d image 0 0 0 0 false
 
-(*quadrillage diagonal gauchebas-droitehaut *)
+(* Diagonal grid right-up/left-down *)
 (* let contour_diag2 image interv =
-   let rec con_d img x y inc_x inc_y  =
-     match (x, y, inc_x, inc_y) with
-	 (x, y, inc_x, inc_y) when inc_x >= (Refe.get_w())-1 -> ()
-       | (x, y, inc_x, inc_y) when inc_y >= (Refe.get_h())-1 && x > (Refe.get_w())-1
-	   -> con_d img inc_x inc_y (inc_x+interv) inc_y
-       | (x, y, inc_x, inc_y) when  inc_y < (Refe.get_h())-1 && (y <= 0)
-	   ->  con_d img 0 inc_y 0 (inc_y+interv)
-       | (x, y, inc_x, inc_y)
-	 -> Sdlvideo.put_pixel_color img x y (0,0,0);
-	   con_d img (x+1) (y-1) inc_x inc_y
+ let rec con_d img x y inc_x inc_y  =
+   match (x, y, inc_x, inc_y) with
+     | (x, y, inc_x, inc_y) when inc_x >= (Refe.get_w())-1 -> ()
+     | (x, y, inc_x, inc_y) when inc_y >= (Refe.get_h())-1 && x > 
+     (Refe.get_w())-1
+      -> con_d img inc_x inc_y (inc_x+interv) inc_y
+     | (x, y, inc_x, inc_y) when  inc_y < (Refe.get_h())-1 && (y <= 0)
+      ->  con_d img 0 inc_y 0 (inc_y+interv)
+     | (x, y, inc_x, inc_y)
+      -> Sdlvideo.put_pixel_color img x y (0,0,0);
+         con_d img (x+1) (y-1) inc_x inc_y
    in con_d image 0 0 0 interv
-*)
+                                 *)
 
 
 
-(* creation de la matrice ayant les points des intersections de la
-   carte *)
-
-
- let rec map_to_mat x y intx inty = match (intx, inty) with
-   | (intx, inty) when intx > ((Refe.get_w())) ->
-       map_to_mat 0 (y+1) 0 (inty + (Refe.get_step()))
-   | (intx, inty) when inty > ((Refe.get_h())) -> ()
-   | (intx, inty) -> Array.set (Refe.get_matrice()).(x) (y) (inty,intx); map_to_mat
-       (x+1) y (intx+((Refe.get_step()))) inty
+(* Creation of matrix which has intersections points of the map *)
+let rec map_to_mat x y intx inty = match (intx, inty) with
+  | (intx, inty) when intx > ((Refe.get_w())) ->
+      map_to_mat 0 (y+1) 0 (inty + (Refe.get_step()))
+  | (intx, inty) when inty > ((Refe.get_h())) -> ()
+  | (intx, inty) -> 
+      Array.set (Refe.get_matrice()).(x) (y) (inty,intx); 
+      map_to_mat (x+1) y (intx + Refe.get_step()) inty
 
 
 
