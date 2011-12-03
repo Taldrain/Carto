698c398f11d4ae3d9d17a88e165ac8f09d08c3f4
alonso giraldo <girald_a@pc-girald-a.(none)>
2011-12-03 18:31:02 +0100

    improve filters

---
+++ src/filter.ml
@@ -0,0 +1,15 @@
+(* FILTER PART of THE PROJECT*)
+
+
+(* image to matrix *)
+
+(* initialization of the matrix filter *)
+
+(* addition of pixel with all neighbours filtered *)
+
+(* multiplication of filter and the matrix *)
+
+(* matrix to image *)
+
+(* ------------------------------------ ------------------------------------- *)
+
